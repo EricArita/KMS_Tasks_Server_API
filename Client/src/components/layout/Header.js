24b131d3dabbba226f575329e865708cae55e15e
KMS_buiphantho <buiphantho@gmail.com>
2020-10-01 15:20:04 +0700

    add dropdown menu for navbar header

--- Client/src/components/layout/Header.js
+++ Client/src/components/layout/Header.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import { FaAffiliatetheme, FaBell } from 'react-icons/fa';
+import { FaRegMoon, FaBell, FaPlus } from 'react-icons/fa';
+import { CgProfile } from 'react-icons/cg';
+import { FiSettings, FiLogOut } from 'react-icons/fi';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 import PropTypes from 'prop-types';
 import { AddTask } from '../AddTask';
 
@@ -10,15 +14,15 @@ export const Header = ({ darkMode, setDarkMode }) => {
   return (
     <header className="header" data-testid="header">
       <nav>
-        <div className="search">
+        <div className="left-navbar">
           <img src="/images/kms-logo-white.svg" alt="KMS Todoist" />
           <div class="search-container">
             <input class="search-box" type="search" placeholder="Search" />
-          </div>  
+          </div>
         </div>
-        <div className="settings">
+        <div className="right-navbar">
           <ul>
-            <li className="settings__add">
+            <li className="right-navbar-add">
               <button
                 data-testid="quick-add-task-action"
                 aria-label="Quick add task"
@@ -28,28 +32,49 @@ export const Header = ({ darkMode, setDarkMode }) => {
                   setShouldShowMain(true);
                 }}
               >
-                +
+                <FaPlus size={21} />
               </button>
             </li>
-            <li className="settings__darkmode">
+            <li className="right-navbar-darkmode">
               <button
                 data-testid="dark-mode-action"
                 aria-label="Darkmode on/off"
                 type="button"
                 onClick={() => setDarkMode(!darkMode)}
               >
-                <FaAffiliatetheme />
+                <FaRegMoon size={22} />
               </button>
             </li>
-            <li className="settings__notification">
+            <li className="right-navbar-notification">
               <button
                 // data-testid="dark-mode-action"
                 // aria-label="Darkmode on/off"
-                // type="button"
-                // onClick={() => setDarkMode(!darkMode)}
+                type="button"
+              // onClick={() => setDarkMode(!darkMode)}
               >
-                <FaBell />
+                <FaBell size={21} />
               </button>
+            </li>
+            <li className="right-navbar-dropdown">
+              <DropdownButton
+                alignRight
+                id="dropdown-menu-align-right"
+              >
+                <Dropdown.Item>
+                  <CgProfile />
+                  <span> Profile</span>
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>
+                  <FiSettings />
+                  <span> Settings</span>
+                  </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>
+                  <FiLogOut />
+                  <span> Log out</span>
+                </Dropdown.Item>
+              </DropdownButton>
             </li>
           </ul>
         </div>
@@ -61,7 +86,7 @@ export const Header = ({ darkMode, setDarkMode }) => {
         showQuickAddTask={showQuickAddTask}
         setShowQuickAddTask={setShowQuickAddTask}
       />
-    </header>
+    </header >
   );
 };
 
