a182e18ae5e0a6a9fd581ec46b6595cabcce2a98
KMS_buiphantho <buiphantho@gmail.com>
2020-10-16 17:49:28 +0700

    update

---
+++ Server/Tasks/Domain/Constants/Constants.cs
@@ -0,0 +1,12 @@
+using static Core.Domain.Constants.Enums;
+
+namespace Core.Domain.Constants
+{
+    public static class Constants
+    {
+        public const string DefaultUsername = "abc";
+        public const string DefaultEmail = "abc@abc";
+        public const string DefaultPassword = "1";
+        public const UserRoles DefaultRole = UserRoles.Administrator;
+    }
+}
