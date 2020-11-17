3944f2b487c3b6d4c4b150a9b450e0388200b6e3
KMS_buiphantho <buiphantho@gmail.com>
2020-11-17 14:19:35 +0700

    add BaseController with the api authentication attribute

---
+++ Server/Tasks/WebApi/Controllers/v1/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.v1
+{
+
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected BaseController() { }
+    }
+}
