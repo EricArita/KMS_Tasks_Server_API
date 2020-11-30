939da17ca2b972c1fdeb814c81fba0ddafaca998
KMS_buiphantho <buiphantho@gmail.com>
2020-11-30 09:37:19 +0700

    update AspNetUser table in DB

--- Server/Tasks/Persistence/Migrations/20201130023311_UpdateDb_3.Designer.cs
+++ Server/Tasks/Persistence/Migrations/20201130023311_UpdateDb_3.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201130023311_UpdateDb_3")]
+    partial class UpdateDb_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
