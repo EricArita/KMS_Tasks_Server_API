5db350b3242b33128fd2adbf28c282e9b71085ce
KMS_buiphantho <buiphantho@gmail.com>
2020-12-01 00:08:20 +0700

    update db

--- Server/Tasks/Application/Models/ApplicationUser.cs
+++ Server/Tasks/Application/Models/ApplicationUser.cs
@@ -5,12 +5,17 @@ namespace Core.Application.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
         public string FirstName { get; set; }
         public string MidName { get; set; }
         public string LastName { get; set; }
-        public string Avatar { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public byte Status { get; set; }
-        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedDate { get; set; }
     }
 }
---
+++ Server/Tasks/DBScript/Db_Script_s7_backend.sql
@@ -0,0 +1,3 @@
+ALTER TABLE [KMS_Tasks].[dbo].[AspNetUsers]
+ALTER COLUMN UserName nvarchar(256);
+Go
