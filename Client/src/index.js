bb82a0d158d2acb81debe75ce45277d4fa2093d1
buidohuy99 <buido.huy99@gmail.com>
2020-12-07 17:19:05 +0700

    Change folder name

--- Client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import 'antd/dist/antd.css';
-import { App } from './App';
-import './App.scss';
-import LoginPage from './pages/LoginPage';
-
-render(<App />, document.getElementById('root'));
-// render(<LoginPage />, document.getElementById('root'));
