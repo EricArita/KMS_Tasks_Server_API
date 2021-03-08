1debdee5c789738b13ba63c7bde442e8d5b5c636
buidohuy99 <buido.huy99@gmail.com>
2021-03-08 18:03:27 +0700

    New route to get users by searching username or email

---
+++ Src/Tasks/MB.Core.Application/Models/User/FindUserByFieldsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MB.Core.Application.Models.User
+{
+    public class FindUserByFieldsModel
+    {
+        [Required]
+        public string UserNameOrEmail { get; set; }
+    }
+}
